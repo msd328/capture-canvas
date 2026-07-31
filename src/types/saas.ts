e62d2600463512eb36ffb1ceccebc50587254016
Dinesh <guruhari32@gmail.com>
2026-07-31 15:06:03 +0200

    Add secure auth readiness types

---
+++ src/types/saas.ts
@@ -0,0 +1,11 @@
+export interface SecureAuthStatus {
+  supported: boolean;
+  signedIn: boolean;
+  storage: "windows-credential-manager" | "unsupported";
+}
+
+export interface SecureAuthProbe {
+  supported: boolean;
+  roundTripOk: boolean;
+  storage: "windows-credential-manager" | "unsupported";
+}
