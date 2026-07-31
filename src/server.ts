da16f55cd988c6c4578d6eff953c7370ad019570
Dinesh <guruhari32@gmail.com>
2026-07-31 15:14:29 +0200

    Route SaaS foundation endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "./lib/error-capture";
 
 import { consumeLastCapturedError } from "./lib/error-capture";
 import { renderErrorPage } from "./lib/error-page";
+import { handleSaasApiRequest } from "./saas/server";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
@@ -47,6 +48,9 @@ function isH3SwallowedErrorBody(body: string): boolean {
 export default {
   async fetch(request: Request, env: unknown, ctx: unknown) {
     try {
+      const saasResponse = await handleSaasApiRequest(request, env);
+      if (saasResponse) return saasResponse;
+
       const handler = await getServerEntry();
       const response = await handler.fetch(request, env, ctx);
       return await normalizeCatastrophicSsrResponse(response);
